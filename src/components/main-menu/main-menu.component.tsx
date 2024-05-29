4947f7e7fa0647fdb8150566db80247b0a85ef78
Mircea Ulmeanu <mircea.ulmeanu@888holdings.com>
2024-05-29 22:26:00 +0300

    To do list

--- src/components/main-menu/main-menu.component.tsx
+++ src/components/main-menu/main-menu.component.tsx
@@ -2,6 +2,10 @@ import {Box} from "@mui/material";
 
 export function MainMenu() {
     return <Box sx={{m: 2}}>
-        Main menu is not implemented yet
+        <p>Main menu is not implemented yet</p>
+        <p>Need to implement</p>
+        <p>List reorder with drag and drop</p>
+        <p>ICY metadata</p>
+        <p>Nicer play-bar with slide up animation</p>
     </Box>;
 }
