1cf2fcf1b9e59201d5baa0aa86a444064f6524f8
Mircea Ulmeanu <mircea.ulmeanu@888holdings.com>
2024-06-13 10:33:26 +0300

    mocked default data updated

--- src/services/streams-list/streams-list.mock-data.ts
+++ src/services/streams-list/streams-list.mock-data.ts
@@ -2,71 +2,72 @@ import {IStream} from "./streams-list.service.interface";
 
 export const streamsListMockData: IStream[] = [
     {
-        "name" : "Rock FM 100.6",
-        // "url" : "http://live.rockfm.ro:9128/rockfm.aacp",
-        "url" : "https://live.rockfm.ro/rockfm.aacp",
+        "name": "Rock FM 100.6",
+        // "url": "http://live.rockfm.ro:9128/rockfm.aacp",
+        "url": "https://live.rockfm.ro/rockfm.aacp",
         "image": "https://www.rockfm.ro/assets/logo/rockfm-white.png"
     },
     {
-        "name" : "Rock FM Hard Rock",
-        "url" : "https://live.rockfm.ro/hard.rock",
+        "name": "Rock FM Hard Rock",
+        "url": "https://live.rockfm.ro/hard.rock",
         "image": "https://media.bauerradio.com/image/upload/c_crop,g_custom/v1606737505/brand_manager/stations/qyxv7nnrvwnnokjxrxgi.jpg"
     },
     {
-        "name" : "Radio Seven 103.4",
-        "url" : "http://80.86.106.32:8000/radio7.mp3",
+        "name": "Radio Seven 103.4",
+        "url": "http://80.86.106.32:8000/radio7.mp3",
         "image": "https://cdn-radiotime-logos.tunein.com/s261203g.png"
     },
     {
-        "name" : "Radio Café",
-        // "url" : "http://live.radiocafe.ro:8048/live.aac",
+        "name": "Radio Café",
+        // "url": "http://live.radiocafe.ro:8048/live.aac",
         "url": "https://live.radiocafe.ro/live.aac",
         "image": "https://radiocafe.ro/wp-content/uploads/2018/12/logo-fara-90fm-resize-e1546368131330.png"
     },
     {
-        "name" : "Magic FM 90.8/93.9",
+        "name": "Magic FM 90.8/93.9",
         // "url": "http://live.magicfm.ro:9128/magicfm.aacp",
         "url": "https://live.magicfm.ro/magicfm.aacp",
         "image": "https://www.magicfm.ro/assets/logo/magicfm.png"
     },
     {
-        "name" : "Smart FM 107.3",
-        // "url" : "http://live.smartradio.ro:9128/live",
+        "name": "Smart FM 107.3",
+        // "url": "http://live.smartradio.ro:9128/live",
         "url": "https://live.smartradio.ro:8443/live",
         "image": "https://myradioonline.ro/public/uploads/radio_img/smart-radio/play_250_250.jpg"
     },
     {
-        "name" : "Radio 90s HITS",
-        "url" : "https://live-bauerdk.sharp-stream.com/DK_HQ_RP06.aac",
+        "name": "Radio 90s HITS",
+        "url": "https://live-bauerdk.sharp-stream.com/DK_HQ_RP06.aac",
         "image": "https://media.bauerradio.com/image/upload/c_crop,g_custom/v1601989516/brand_manager/stations/llhkjewdg3glk1yebysr.png"
     },
+    // {
+    //     "name": "Rock Radio 1",
+    //     "url": "http://144.217.77.176:8000/;",
+    //     "image": "http://www.rockradio1.com/modules/Forums/images/avatars/blank.gif"
+    // },
+    // {
+    //     "name": "Heavy Metal Radio",
+    //     "url": "http://hmr.heavymetalradio.com:8000",
+    //     "image": "http://www.heavymetalradio.ro/themes/HeavyMetalRadio/images/hd/logo.png"
+    // },
     {
-        "name" : "Rock Radio 1",
-        "url" : "http://144.217.77.176:8000/;",
-        "image": "http://www.rockradio1.com/modules/Forums/images/avatars/blank.gif"
-    },
-    {
-        "name" : "Heavy Metal Radio",
-        "url" : "http://hmr.heavymetalradio.com:8000",
-        "image": "http://www.heavymetalradio.ro/themes/HeavyMetalRadio/images/hd/logo.png"
-    },
-    {
-        "name" : "ChroniX Metalcore",
-        "url" : "https://usa17.fastcast4u.com/proxy/chronixr?mp=/1",
+        "name": "ChroniX Metalcore",
+        "url": "https://usa17.fastcast4u.com/proxy/chronixr?mp=/1",
         "image": "https://storage.googleapis.com/production-domaincom-v1-0-5/365/37365/g6iHDsAr/b85148fcf1ac49358b91acb3fd11085e"
     },
     {
-        "name" : "ChroniX Aggression",
-        "url" : "https://usa17.fastcast4u.com/proxy/gebacher?mp=/1",
+        "name": "ChroniX Aggression",
+        "url": "https://usa17.fastcast4u.com/proxy/gebacher?mp=/1",
         "image": "https://storage.googleapis.com/production-domaincom-v1-0-5/365/37365/g6iHDsAr/7dcb25b5a6b64d28b91eb518dc24e59f"
     },
-    {
-        "name" : "Sport Total FM",
-        "url" : "http://84.247.84.202:8001"
-    },
-    {
-        "name" : "Buzz FM Buzau 87.9",
-        "url" : "http://88.158.124.28:81/;",
-        "image": "http://radio.tvbuzau.ro/wp-content/uploads/2016/06/cropped-buzz-mic-2-192x192.gif"
-    },
+    // {
+    //     "name": "Sport Total FM",
+    //     "url": "http://84.247.84.202:8001"
+    // },
+    // {
+    //     "name": "Buzz FM Buzau 87.9",
+    //     "url": "http://88.158.124.28:81/;",
+    //     // "image": "http://radio.tvbuzau.ro/wp-content/uploads/2016/06/cropped-buzz-mic-2-192x192.gif"
+    //     "image": "https://radio.tvbuzau.ro/wp-content/uploads/2019/01/sigla-buzz-copy.jpg"
+    // },
 ];
