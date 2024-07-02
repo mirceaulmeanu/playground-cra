e3c538a816087573bfc5285f37585418ec0dbe01
Mircea Ulmeanu <mircea.ulmeanu@888holdings.com>
2024-07-02 13:05:53 +0300

    temporary update sw button in the menu

--- src/components/main-menu/main-menu.component.tsx
+++ src/components/main-menu/main-menu.component.tsx
@@ -1,5 +1,6 @@
 import {Box, ListItemIcon, ListItemText, MenuItem, MenuList} from "@mui/material";
 import AddIcon from '@mui/icons-material/Add';
+import UpdateIcon from '@mui/icons-material/Update';
 import {useCallback} from "react";
 import {useServices} from "../../react-hooks/use-services.hook";
 
@@ -8,6 +9,16 @@ export function MainMenu() {
     const newStream = useCallback(() => {
         services.streamForm.newStream();
     }, [services.streamForm]);
+    const skipWaitingNewWorker = useCallback(() => {
+        if ('serviceWorker' in navigator) {
+            navigator.serviceWorker.ready.then((registration) => {
+                if (registration.waiting) {
+                    // send the skip message to kick off the service worker install.
+                    registration.waiting.postMessage('SKIP_WAITING');
+                }
+            });
+          }
+    }, []);
     return <Box sx={{m: 2}}>
         <MenuList id="basic-menu">
             <MenuItem onClick={newStream}>
@@ -16,6 +27,12 @@ export function MainMenu() {
                 </ListItemIcon>
                 <ListItemText>ADD NEW STREAM</ListItemText>
             </MenuItem>
+            { services.serviceWorker.isNewServiceWaiting ? <MenuItem onClick={skipWaitingNewWorker}>
+                <ListItemIcon>
+                    <UpdateIcon fontSize="small" />
+                </ListItemIcon>
+                <ListItemText>UPDATE SERVICE WORKER</ListItemText>
+            </MenuItem> : null }
         </MenuList>
         <p></p>
         <p>Need to implement</p>
@@ -27,6 +44,7 @@ export function MainMenu() {
             <li>Add list export/import capability</li>
             <li>Add PWA</li>
             <li>Add validation for https:// stream and image url</li>
+            <li>Change the update button from the menu to also a notification</li>
         </ul>
     </Box>;
 }
